47c825322bcffcbf094aad7a1b671b30716f27e3
Avishay Ashkenazi <aavishay@gmail.com>
2026-03-30 00:36:16 +0300

    fix: remove bottle :unneeded (breaks Homebrew 5.x) and fix install path

--- cloudviz.rb
+++ cloudviz.rb
@@ -6,7 +6,6 @@ class Cloudviz < Formula
   sha256 "e948180002497cd4f1b614b2cafc23a67cfb676ddd4a199af324af3861bf3047"
 
   url "https://github.com/aavishay/cloudviz/releases/download/v0.3.0/cloudviz_0.3.0_darwin_arm64.tar.gz"
-  bottle :unneeded
 
   def install
     bin.install "cloudviz"
