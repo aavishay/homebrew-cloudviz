6f92ffce476682257044d279be96bdf26ef16546
Avishay Ashkenazi <aavishay@gmail.com>
2026-03-30 00:05:14 +0300

    fix: correct SHA256 checksums for v0.3.0 release

--- cloudviz.rb
+++ cloudviz.rb
@@ -7,11 +7,11 @@ class Cloudviz < Formula
   on_macos do
     on_arm do
       url "https://github.com/aavishay/cloudviz/releases/download/v0.3.0/cloudviz_0.3.0_darwin_arm64.tar.gz"
-      sha256 "80aa55a9a9cd10b09a0583ef2c229aeee7a5f94aab3490e15b3e5f3521669158"
+      sha256 "e948180002497cd4f1b614b2cafc23a67cfb676ddd4a199af324af3861bf3047"
     end
     on_intel do
       url "https://github.com/aavishay/cloudviz/releases/download/v0.3.0/cloudviz_0.3.0_darwin_amd64.tar.gz"
-      sha256 "5eae539985b81b64e99ca1d838813d8d635ec92a1554e74d5a0410061f87221c"
+      sha256 "50087e8a2067f46ffee1c6b0235d336f46894b37fc01f6f28272c1710469945b"
     end
   end
 
